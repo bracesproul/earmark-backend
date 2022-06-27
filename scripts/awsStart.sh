7cd88ec363bcfbb1935faf9d03b062fba75aa6ec
Brace Sproul <braceasproul@gmail.com>
2022-06-27 20:27:43 +0100

    updated app timeout in appspec.yml to lower times

--- scripts/awsStart.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ec2-user/earmark-backend
-npm i
-npm run clean
-npm run build
-npm run start
--- scripts/localStart.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-npm i
-npm run clean
-npm run build
-npm run start
